bd986d73e741c6b8f10f5fa02403d9406156a159
Mauz Khan <115772334+mauzkhan13@users.noreply.github.com>
2024-11-08 19:15:51 +0500

    Update google_reviews.py

--- google_reviews.py
+++ google_reviews.py
@@ -1,5 +1,5 @@
 
-# import undetected_chromedriver as uc
+import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
